cd5853a78abfffcb45b7e1639f776fa7ad934f41
Wiley <wileystphillips@gmail.com>
2021-10-06 23:50:08 -0400

    Initial commit and main class

---
+++ src/com/wileyphillips/oopchallenge/Main.java
@@ -0,0 +1,11 @@
+package com.wileyphillips.oopchallenge;
+
+
+public class Main {
+    public static void main(String[] args) {
+        options();
+    }
+    public static void options () {
+        System.out.println("1. Left Eye \r\n 2. Right Eye \r\n 3. Heart \r\n 4. Stomach \r\n 5. Skin \r\n 6. Quit");
+    }
+}
