e0c1fa8adb6c539a3730b9537c48b04c11630fed
Wiley <wileystphillips@gmail.com>
2021-10-10 23:53:45 -0400

    Add a scanner and functionality to the method options In main I made it so the user's input would be taken until they selected one of the six options.

--- src/com/wileyphillips/oopchallenge/Main.java
+++ src/com/wileyphillips/oopchallenge/Main.java
@@ -1,11 +1,27 @@
 package com.wileyphillips.oopchallenge;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         options();
     }
     public static void options () {
-        System.out.println("1. Left Eye \r\n2. Right Eye\r\n3. Heart \r\n4. Stomach \r\n5. Skin \r\n6. Quit");
+        String[] numOptions = {"1","2","3","4","5","6"};
+        Scanner scanner = new Scanner(System.in);
+        boolean found = false;
+        while (! found) {
+            System.out.println("1. Left Eye \r\n2. Right Eye\r\n3. Heart \r\n4. Stomach \r\n5. Skin \r\n6. Quit");
+            String choice = scanner.next();
+            System.out.println(choice);
+            for (int i=0; i<numOptions.length; i++) {
+                if (numOptions[i].equals(choice)) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        System.out.println("Found");
     }
 }
