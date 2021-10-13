a040f6a0f50c0493b03d38c5d0da20591b55d721
Wiley <wileystphillips@gmail.com>
2021-10-13 05:33:41 -0400

    Utilize an enhanced for loop

--- src/com/wileyphillips/oopchallenge/Main.java
+++ src/com/wileyphillips/oopchallenge/Main.java
@@ -7,8 +7,12 @@ public class Main {
     public static void main(String[] args) {
         //Patient has the attributes found in the example.
         Patient patient = new Patient("Tom", 25, new LeftEye("Short sighted", "Blue"), new RightEye("Normal", "Blue"), new Heart("Normal", 65), new Stomach("PUD"), new Skin("Burned"));
+        System.out.println("Name: " + patient.getName() + "\nAge: " + patient.getAge());
         options();
     }
+//    public static void chooseLeft() {
+//        System.out.println("Name: Left Eye\nMedical Condition: "+ patient.);
+//    }
     public static void options () {
         String[] numOptions = {"1","2","3","4","5","6"};
         Scanner scanner = new Scanner(System.in);
@@ -17,8 +21,8 @@ public class Main {
             System.out.println("1. Left Eye \r\n2. Right Eye\r\n3. Heart \r\n4. Stomach \r\n5. Skin \r\n6. Quit");
             String choice = scanner.next();
             System.out.println(choice);
-            for (int i=0; i<numOptions.length; i++) {
-                if (numOptions[i].equals(choice)) {
+            for (String number: numOptions) {
+                if (number.equals(choice)) {
                     found = true;
                     break;
                 }
