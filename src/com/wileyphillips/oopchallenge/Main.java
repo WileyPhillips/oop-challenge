086e00c55728ef18d9c81332bf4b6a378a25f044
Wiley <wileystphillips@gmail.com>
2021-10-14 23:49:31 -0400

    Began a temporary solution for body part "methods" Until I can figure out a more sophisticated solution a series of if statements will have to do as far as returning patient information.

--- src/com/wileyphillips/oopchallenge/Main.java
+++ src/com/wileyphillips/oopchallenge/Main.java
@@ -8,26 +8,32 @@ public class Main {
         //Patient has the attributes found in the example.
         Patient patient = new Patient("Tom", 25, new LeftEye("Short sighted", "Blue"), new RightEye("Normal", "Blue"), new Heart("Normal", 65), new Stomach("PUD"), new Skin("Burned"));
         System.out.println("Name: " + patient.getName() + "\nAge: " + patient.getAge());
-        options();
+        while (true) {
+            String number = options();
+            System.out.println(number);
+            //Still not sure of a proper solution, so I will be brute forcing it for the moment.
+            if (number.equals("1")){
+                System.out.println("Name: Left Eye\nMedical Condition: " + patient.getLeftEye().getMedicalCondition() + "\nColor: " + patient.getLeftEye().getColor());
+            } else if (number.equals("2")) {
+                System.out.println("Name: Left Eye\nMedical Condition: " + patient.getRightEye().getMedicalCondition() + "\nColor: " + patient.getRightEye().getColor());
+            }
+        }
+
     }
 //    public static void chooseLeft() {
 //        System.out.println("Name: Left Eye\nMedical Condition: "+ patient.);
 //    }
-    public static void options () {
+    public static String options () {
         String[] numOptions = {"1","2","3","4","5","6"};
         Scanner scanner = new Scanner(System.in);
-        boolean found = false;
-        while (! found) {
+        while (true) {
             System.out.println("1. Left Eye \r\n2. Right Eye\r\n3. Heart \r\n4. Stomach \r\n5. Skin \r\n6. Quit");
             String choice = scanner.next();
-            System.out.println(choice);
             for (String number: numOptions) {
                 if (number.equals(choice)) {
-                    found = true;
-                    break;
+                    return number;
                 }
             }
         }
-        System.out.println("Found");
     }
 }
